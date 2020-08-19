f8e50cf1f62846562236ae71b31046fdc2cc7606
Soumya0753 <satpathysoumya13@gail.com>
2020-08-19 15:39:42 -0700

    Again committed

---
+++ MyJavaProject/src/AddMethod.java
@@ -0,0 +1,15 @@
+
+public class AddMethod {
+
+	public static void main(String[] args) {
+	add(10,50);
+
+	}
+
+	private static void add(int i, int j) 
+	{
+	System.out.println("Addition is :" +(i+j));
+		
+	}
+
+}
